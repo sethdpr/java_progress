fb95b43c9de3ab894a7091d9f6a54acb7aa908f2
sethdpr <156798514+sethdpr@users.noreply.github.com>
2024-01-22 11:30:29 +0100

    Delete Dieren_package directory

--- Dieren_package/Slang.java
+++
@@ -1,9 +0,0 @@
-package Dieren_package;
-public class Slang extends Dier {
-    public Slang(String naam){
-        super(naam);
-    }
-    public void vervel(){
-        System.out.println(getNaam() + ": ik vervel");
-    }
-}
